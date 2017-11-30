aef79c9ec96e1cc47f8bacee9e30b91d92a1c19d
ADMIN <ADMIN@ANVESHREDDY.AITHA>
2017-12-01 00:21:12 +0530

    1st file change

---
+++ src/main/java/com/app/interceptor/TimerInterceptor.java
@@ -0,0 +1,30 @@
+package com.app.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class TimerInterceptor extends HandlerInterceptorAdapter {
+	private long x,y,z;
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("preHandle()..");
+		x=System.currentTimeMillis();
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle()..");
+		y=System.currentTimeMillis();
+		z=y-x;
+		request.setAttribute("diff", z);
+		
+	}
+	
+}
